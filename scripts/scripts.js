3bfc6028caa15806c6a5ea08e225939af4c57899
Harrison Lizarazo <35266866+hsl39@users.noreply.github.com>
2020-11-18 13:31:01 -0500

    Random Prompt

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+function newPrompt(){
+	
+	// returns a random integer from 0 to 2
+	var rand = Math.floor(Math.random() * 3); 
+	document.getElementById("prompt").innerHTML = testPrompts[rand]["prompt"];
+	
+}
+
+
+
+
+var testPrompts = [
+	{"prompt" : "Party with the gang!!!"},
+	{"prompt" : "Second Prompt"	},
+	{"prompt" : "Third Prompt"}
+]
